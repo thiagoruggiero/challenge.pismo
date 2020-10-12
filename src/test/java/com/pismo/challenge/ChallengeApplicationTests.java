0291326d6abcf2843092eeff1b288fe6d2aa3e48
Thiago Ruggiero <thiago.ruggiero@avenuecode.com>
2020-10-12 19:08:07 -0300

    adding test

--- src/test/java/com/pismo/challenge/ChallengeApplicationTests.java
+++ src/test/java/com/pismo/challenge/ChallengeApplicationTests.java
@@ -1,13 +1,47 @@
 package com.pismo.challenge;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pismo.challenge.entity.Account;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ChallengeApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void GetAccountTest() throws Exception {
+		Account account = new Account();
+		account.setDocumentNumber("12345");
+		mockMvc.perform(MockMvcRequestBuilders.post("/accounts")
+				.content(asJsonString(account))
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+
+		this.mockMvc.perform(get("/accounts/1")).andDo(print()).andExpect(status().isOk());
 	}
 
+	public static String asJsonString(final Object obj) {
+		try {
+			final ObjectMapper mapper = new ObjectMapper();
+			final String jsonContent = mapper.writeValueAsString(obj);
+			return jsonContent;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
